e6973ed6da0d16f38755988284f9af3233751aaf
Maxim Koblyuk <89156031+MaximKoblyuk@users.noreply.github.com>
2023-04-24 15:11:37 +0200

    Create dublicates.py

---
+++ dublicates.py
@@ -0,0 +1,14 @@
+ex_list = [1, 2, 3, 1]
+contains_dublicates(ex_list) = True
+  if ex_list = [1, 5555, 0, 7]
+    contains_dublicates(ex_list) = False
+       elif ex_list = []
+        contains_dublicates(ex_list) = False
+        
+        
+def contains_dublicates(ex_list) :
+  return len(set(ex_list)) != len(ex_list)
+
+  ex_list = [1, 2, 3, 1]
+  
+print (contains_dublicates(ex_list))
